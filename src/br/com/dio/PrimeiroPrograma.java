855dbac7997be9e40a9a96ef48866d9d157ab737
Rubinelle <rubinelle@gmail.com>
2022-10-25 21:45:58 -0300

    alterando o nome do projeto

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,12 +6,55 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        Livros livros = new Livros();
+
         System.out.println(gato);
+        System.out.println(livros);
+
+        Livros livro1 = new Livros( "O problema dos 3 corpos",  300);
 
 
-      /*  int a =5;
+      /* int a =5;
         int b =3;
         System.out.println("Hello Wolrd! " + (a+b));*/
+    }
+}
+
+class Livros {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livros(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public Livros() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
 
